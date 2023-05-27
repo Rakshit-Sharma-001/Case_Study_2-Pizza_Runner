508c5c849b9b6138aa0bac338d10ad4fa20b2a3f
Rakshit Sharma <133633861+Rakshit-Sharma-001@users.noreply.github.com>
2023-05-27 13:47:05 +0530

    Update Part E (Bonus Question).sql

--- Part E (Bonus Question).sql
+++ Part E (Bonus Question).sql
@@ -1,5 +1,5 @@
 -- Q: If Danny wants to expand his range of pizzas - how would this impact the existing data design? Write an INSERT statement 
-	# to demonstrate what would happen if a new Supreme pizza with all the toppings was added to the Pizza Runner menu?
+	-- to demonstrate what would happen if a new Supreme pizza with all the toppings was added to the Pizza Runner menu?
 
 insert into pizza_names value(3,'Supreme');
 select * from pizza_names;
